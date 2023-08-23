40a0054a4224d296f0e30f5e6037b0c9c86187b8
patrickhulten <142944872+patrickhulten@users.noreply.github.com>
2023-08-23 02:46:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+let myHTTP = require("http");
+
+
+let myserver = myHTTP.createServer( 
+  function( myrequest, myresponse ) {
+
+    let mytext;
+    if ( myrequest.url === "/speakeasy" ) {
+      mytext = "Come on in";
+    } else {
+      mytext = "Who sent you?";
+    }
+
+
+   
+    myresponse.writeHead( 200, { "Content-Type": "text/plain" } );
+
+    myresponse.end( mytext + "\n" );
+  }
+
+ );
+
+myserver.listen(8080, "0.0.0.0");
+
+console.log("server has started");
